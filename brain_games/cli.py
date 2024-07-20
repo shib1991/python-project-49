661264909f2b3fc779c1c28e46617605a827cf28
shorov_ibragim <sh.ibragim@gmail.com>
2024-07-20 07:03:49 -0500

    builded new version brain_games and reinstall

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,4 @@ def welcome_user():
     import prompt
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-welcome_user()
+    return print(f'Hello, {name}')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,5 +3,6 @@ from brain_games.cli import welcome_user
 def main():
     welcome_user()
 
+    
 if __name__ == '__main__':
     main()
